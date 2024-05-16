8fb03add39518b493bde06561db1f0ad56969bf1
rootkiti <robertu20002000@gmail.com>
2024-05-16 07:03:44 +0200

    top navigations

---
+++ includes/topNav.php
@@ -0,0 +1,17 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>
+        <span class="icon-bar"></span>                        
+      </button>
+    </div>
+    <div class="collapse navbar-collapse" id="myNavbar">
+      <ul class="nav navbar-nav">
+        <li><a href="#">Crop Recommendation</a></li>
+        <li><a href="#">Seeds Calculation</a></li>
+      </ul>
+    </div>
+  </div>
+</nav>
--- index.php
+++ index.php
@@ -116,23 +116,9 @@ $crop = '';
     </style>
 </head>
 <body>
-<nav class="navbar navbar-inverse">
-  <div class="container-fluid">
-    <div class="navbar-header">
-      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#myNavbar">
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>
-        <span class="icon-bar"></span>                        
-      </button>
-    </div>
-    <div class="collapse navbar-collapse" id="myNavbar">
-      <ul class="nav navbar-nav">
-        <li><a href="#">Crop Recommendation</a></li>
-        <li><a href="#">Seeds Calculation</a></li>
-      </ul>
-    </div>
-  </div>
-</nav>
+<?php
+include('includes/topNav.php');
+?>
 
 <form method='post'>
     <table class="table">
